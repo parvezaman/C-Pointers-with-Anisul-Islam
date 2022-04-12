81d92d35dae02f6addd25f53ae8a1192a1f02550
parvezaman <emailtoamanullah@gmail.com>
2022-04-12 22:49:26 +0600

    swapping numbers using pointers

--- swapingTwoNumbersByPointers.c
+++ swapingTwoNumbersByPointers.c
@@ -4,6 +4,10 @@ int main()
     int x = 10, y = 20, temporary;
     int *pointer_x, *pointer_y;
 
+    printf("before\n");
+    printf("x = %d\n", x);
+    printf("y = %d\n", y);
+
     pointer_x = &x;
     pointer_y = &y;
 
@@ -11,6 +15,7 @@ int main()
     *pointer_x = *pointer_y;
     *pointer_y = temporary;
 
+    printf("after\n");
     printf("x = %d\n", x);
     printf("y = %d\n", y);
 
